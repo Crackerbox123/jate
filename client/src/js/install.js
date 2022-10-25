e52180e71fa8f84d6455a51a9cc84b0b4e1be669
Kal O <koestreicher4@gmail.com>
2022-10-24 20:32:03 -0500

    fixes

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -16,5 +16,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-  console.log('✅ app installed', event);
+  console.log('app installed', event);
 });
